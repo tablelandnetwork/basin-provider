1fbea5d34f795f4cca449efb30fefb3087aae416
Sander Pick <sanderpick@gmail.com>
2023-08-30 10:27:11 -0700

    add health check
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- lib/worker/src/main.rs
+++ lib/worker/src/main.rs
@@ -6,24 +6,41 @@ use basin_protocol::publications;
 use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
 use clap::Parser;
 use futures::{AsyncReadExt, FutureExt};
+use serde::Serialize;
 use sqlx::postgres::PgPool;
-use std::net::SocketAddr;
+use std::{convert::Infallible, net::SocketAddr};
+use warp::{http::StatusCode, Filter, Rejection, Reply};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(long)]
     database_url: String,
 
-    #[arg(short, long)]
+    #[arg(long)]
     bind_address: SocketAddr,
+
+    #[arg(long)]
+    bind_health_address: SocketAddr,
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let pool = PgPool::connect(&args.database_url).await?;
-    listen(args.bind_address, pool).await
+
+    let warp_server = warp::serve(
+        warp::path("health")
+            .map(warp::reply)
+            .recover(handle_warp_rejection),
+    )
+    .run(args.bind_health_address);
+    tokio::spawn(async { warp_server.await });
+
+    listen(
+        args.bind_address,
+        PgPool::connect(&args.database_url).await?,
+    )
+    .await
 }
 
 pub async fn listen(addr: SocketAddr, pool: PgPool) -> Result<(), Box<dyn std::error::Error>> {
@@ -54,4 +71,27 @@ pub async fn listen(addr: SocketAddr, pool: PgPool) -> Result<(), Box<dyn std::e
             }
         })
         .await
+}
+
+#[derive(Serialize)]
+struct ErrorMessage {
+    code: u16,
+    message: String,
+}
+
+async fn handle_warp_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
+    let (code, message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Internal Server Error".to_string(),
+        )
+    };
+    let json = warp::reply::json(&ErrorMessage {
+        code: code.as_u16(),
+        message,
+    });
+    Ok(warp::reply::with_status(json, code))
 }
