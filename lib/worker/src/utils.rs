f78972103c6a6624d2308695b673a0afa42ad2fd
Sander Pick <sanderpick@gmail.com>
2023-09-13 17:09:38 -0700

    adds basic e2e test
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

---
+++ lib/worker/src/utils.rs
@@ -0,0 +1,36 @@
+use basin_protocol::tx;
+use capnp::{data, message, private::units::BYTES_PER_WORD};
+use ethers::types::Address;
+
+/// Recovers address from tx:Reader
+pub fn recover_addr(tx: tx::Reader, sig: data::Reader) -> capnp::Result<Address> {
+    if sig.len() != 65 {
+        return Err(capnp::Error::failed("signature must be 65 bytes".into()));
+    }
+    let payload = canonicalize_tx(tx)?;
+    let hash = crate::crypto::keccak256(payload.as_slice());
+    let addr = crate::crypto::recover(hash.as_slice(), &sig[..64], sig[64] as i32)?;
+    Ok(addr)
+}
+
+/// Returns the canonical bytes of tx::Reader
+pub fn canonicalize_tx(reader: tx::Reader) -> capnp::Result<Vec<u8>> {
+    let size = reader.total_size()?.word_count + 1;
+    let mut message =
+        message::Builder::new(message::HeapAllocator::new().first_segment_words(size as u32));
+    message.set_root_canonical(reader)?;
+    let output_segments = message.get_segments_for_output();
+    if output_segments.len() != 1 {
+        return Err(capnp::Error::failed(format!(
+            "canonical tx has {} segments; expected 1",
+            output_segments.len()
+        )));
+    }
+    let output = output_segments[0];
+    if (output.len() / BYTES_PER_WORD) as u64 > size {
+        return Err(capnp::Error::failed(format!(
+            "canonical tx size must be less than {size}"
+        )));
+    }
+    Ok(output.to_vec())
+}
